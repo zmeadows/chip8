ffcf901c45aa7f60531e2374c95c72aa08cf3911
Zachary Meadows <zmeadows@gmail.com>
2020-03-24 13:59:12 -0700

    removed old un-necessary files

--- build_and_run.sh
+++
@@ -1,3 +0,0 @@
-FLAGS="-std=c++2a -lSDL2 -g -march=native -Wall -Wextra -fno-exceptions -fno-rtti -pedantic -Wno-narrowing -Werror -Wno-unused-parameter -Wno-unused-variable"
-
-clang++-10 main.cpp -o chip8 ${FLAGS} && ./chip8
--- build_and_test.sh
+++
@@ -1,7 +0,0 @@
-FLAGS="-std=c++2a -g -march=native -Wall -Wextra -fno-exceptions -fno-rtti -pedantic -Wno-narrowing -Werror -Wno-unused-parameter -Wno-unused-variable"
-
-clang++-10 main.cpp -o chip8 ${FLAGS}
-echo "emulator build successful"
-clang++-10 chip8_tests.cpp -o chip8_tests ${FLAGS}
-echo "test build successful"
-./chip8_tests
