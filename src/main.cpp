71825d702aaf6a88d3cffd214b83d924774ebe3a
Zachary Meadows <zmeadows@gmail.com>
2020-03-24 20:53:29 -0700

    seed random number generator for Cxkk opcode

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 #include "chip8_glfw.hpp"
 
 #include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
 int main(void)
 {
+    srand((unsigned)time(NULL));
+
     chip8::init();
 
-    auto emu = chip8::core::create_emulator("./roms/pong.rom");
+    auto emu = chip8::core::create_emulator("./roms/BC_test.ch8");
 
     while (true) {
         const bool game_finished = chip8::tick(emu);
