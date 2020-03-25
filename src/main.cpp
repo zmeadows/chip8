2ff090da201899efe8802ca97cd2397b9987c6e3
Zachary Meadows <zmeadows@gmail.com>
2020-03-25 09:36:58 -0700

    clean un-used includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include "chip8.hpp"
-#include "chip8_core.hpp"
-#include "chip8_glfw.hpp"
 
 #include <cstdio>
 
@@ -10,6 +8,7 @@ int main(void)
 
     char rom_path[512];
     sprintf_s(rom_path, 512, "%s/roms/BC_test.ch8", CHIP8_ASSETS_DIR);
+
     auto emu = chip8::core::create_emulator(rom_path);
 
     while (true) {
