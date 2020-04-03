2a88b68c67237466580bc722614ea30344166fc2
Zachary Meadows <zmeadows@gmail.com>
2020-04-03 09:59:47 -0700

    empty disassembler file added

---
+++ src/chip8_disassembler.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace chip8::disassembler {
+
+// constexpr auto maximum_line_length = 32;
+//
+// constexpr auto max_prog_instr_count =
+//
+//     std::string disassemble_opcode(uint16_t opcode);
+
+} // namespace chip8::disassembler
